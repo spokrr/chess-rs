b51f4e4fab88af3064a99a29e5f53c16ed3e510b
Jay Schuster <jmsq3m@rc29xcs213.managed.mst.edu>
2022-10-11 10:12:10 -0500

    created the chess files

---
+++ src/chess.rs
@@ -0,0 +1,16 @@
+
+pub struct Board {
+
+}
+
+impl Board {
+
+    pub fn new() -> Self {
+
+        let board: Board = Board::new();
+        board
+        
+    }
+
+}
+
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod chess;
+
+use crate::chess::Board;
+
+fn main() {
+
+    println!("Hello, world!");
+    let b = Board::new();
+
+}
